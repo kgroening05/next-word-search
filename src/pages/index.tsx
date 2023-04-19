a3efeeefc98142e03faef1f0ee1d322233068986
Kyle Groening <kylegroening@gmail.com>
2023-04-19 16:22:22 -0400

    Add text area to enter search targets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,18 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState } from 'react'
 import CrosswordGrid from '../components/crosswordGrid';
-
+import WordHighlighter from '../components/wordHighlighter'
 export default function Home() {
   const [rawPuzzle, setRawPuzzle] = useState('')
+  const [rawTargets, setRawTargets] = useState('')
 
   function handlePuzzleInput(event:any) {
     setRawPuzzle(event.target.value)
   }
-
   
+  function handleTargetInput(event:any) {
+    setRawTargets(event.target.value)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -25,9 +28,22 @@ export default function Home() {
         onChange={(e) => handlePuzzleInput(e)} 
         placeholder='Enter word-search here'
       />
+      <textarea
+        name="targets" 
+        id="targets"
+        cols={30}
+        rows={10}
+        placeholder='Enter words to find'
+        onChange={(e) => handleTargetInput(e)}
+      />
       <section>
-          <CrosswordGrid puzzle={rawPuzzle} />
+        {
+          rawPuzzle
+          ? <CrosswordGrid puzzle={rawPuzzle} />
+          : null
+        }
       </section>
+      <WordHighlighter rawTargets={rawTargets}/>
     </main>
   )
 }
