de32fa3a8f898b12718db756bb96b7b16ef575d8
Kyle Groening <kylegroening@gmail.com>
2023-04-18 20:48:19 -0400

    Add crossword grid component

---
+++ src/components/crosswordGrid.js
@@ -0,0 +1,22 @@
+import { parseFormInputs } from "../../lib/wordSearchHandler"
+
+
+export default function CrosswordGrid({ puzzle }) {
+    const grid = parseFormInputs(puzzle)
+    console.log({grid})
+    const gridRows = [];
+    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+        const row = grid[rowIndex];
+        const letters = [];
+        for (let letterIndex = 0; letterIndex < row.length; letterIndex++) {
+            const letter = row[letterIndex];
+            letters.push(<td className="p-1 text-center">{letter}</td>)
+        }
+        gridRows.push(<tr>{letters}</tr>)
+    }
+    return (
+        <table className="table-fixed p">
+            {gridRows}
+        </table>
+    )
+}
